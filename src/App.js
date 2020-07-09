4ee427d63351b43d7fd4c750b23d0848c1fd0404
Dawid Rynkal <dawidos14051997@gmail.com>
2020-07-09 19:39:04 +0200

    Create Result and Results components and use Axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Search from './components/Search';
+import axios from 'axios';
+import Results from './components/Results'
 
 function App() {
+  const [state, setState] = useState({
+    s: "",
+    result: [],
+    selected: {},
+  })
+
+  const apiurl = "http://www.omdbapi.com/?i=tt3896198&apikey=e0f1a238";
+
+  const search = e => {
+    if (e.key === "Enter") {
+      axios(apiurl + "&s=" + state.s).then(({ data }) => {
+        let results = data.Search;
+
+        setState(prevState => {
+          return {
+            ...prevState, result: results,
+          }
+
+        })
+      })
+    }
+  }
+
+  console.log(state.result)
+
+  const handleInput = e => {
+    let s = e.target.value;
+
+    setState(prevState => ({
+      ...prevState,
+      s: s,
+    }))
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>movie's </h1>
+        <Search
+          handleInput={handleInput}
+          search={search}
+
+        />
+        <Results
+          results={state.result}
+        />
       </header>
     </div>
   );
